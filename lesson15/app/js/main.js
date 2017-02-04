6fcf0bb0fb491c4e2028cd2fc06bc4e10aecd11f
Arthur404 <ananarben@gmail.com>
2017-02-04 14:06:12 +0200

    init lesson15

---
+++ lesson15/app/js/main.js
@@ -0,0 +1,21 @@
+/*$('.nav-menu').css({'height':'0'});
+ $('#close-nav').addClass("remove-btn")
+
+ $('#close-nav').click(function () {
+ $('.nav-menu').animate({'height':'0'},300);
+ $('#open-nav').removeClass("remove-btn");
+ $('#close-nav').addClass("remove-btn")
+ });
+
+ $('#open-nav').click(function () {
+ $('.nav-menu').animate({'height':'100vh'},300);
+ $('#close-nav').removeClass("remove-btn");
+ $('#open-nav').addClass("remove-btn")
+ });*/
+
+//slider
+$('.slider').slick({
+    dots: true,
+    arrows: false,
+    cssEase: 'ease-in'
+});
