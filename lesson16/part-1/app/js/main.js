6b3cef9442574457c1318866510ce9506a36389c
Arthur404 <ananarben@gmail.com>
2017-02-08 17:59:03 +0200

    part-1 added

--- lesson16/part-1/app/js/main.js
+++ lesson16/part-1/app/js/main.js
@@ -1,69 +1,113 @@
 $('.text-in').fadeOut();
 $('.show-out').hide();
+
 $(document).ready(function () {
+
     $('.btn-new-class').on('click',function(){
         $(this).addClass('new-class')
     });
+
     $('.btn-remove-class').on('click',function(){
         $(this).removeClass('btn-remove-class')
     });
+
     $('.btn-toggle-class').on('click',function(){
         $(this).toggleClass('btn-toggle-class')
     });
+
     $('.btn-get-attr').on('click',function(){
         alert("Получен атрибут class с именем: " + getattr.getAttribute('class'))
     });
+
     $('.btn-set-attr').on('click',function(){
         setattr.setAttribute('class', 'btn-click new-class')
     });
+
     $('.alert').on('click',function(){
         alert($(this).text())
     });
+
     $('.trigger').on('click',function(){
         $('.alert').trigger('click')
     });
+
     $('.clone').on('click',function(){
         var clone = $(this).clone().text("(cloned)").appendTo('.clone');
         clone.css("margin-left", "150px").css("margin-top", "-35px");
     });
+
     $('.closest').on('click',function(){
-        console.log($(this).closest('li'))
+        console.log($(this).closest('li').prop('tagName').toLowerCase())
     });
+
     $('.each-btn').on('click',function(){
         var vals=$("button").map(function(){
             return $(this).text();
         });
         console.log(vals.get());
     });
+
     $(".find").on('click',function(){
         var find = $("body").find(".find");
         console.log(find);
     });
+
     $('.fade-in').on('click',function(){
-        $('.text-in').fadeIn()
+        $('.text-in').fadeIn(700)
     });
+
     $('.fade-out').on('click',function(){
-        $('.text-out').fadeOut()
+        $('.text-out').fadeOut(300)
     });
+
     $('.hide').on('click',function(){
-        $('.hide-in').hide()
+        $('.hide-in').hide(500)
     });
+
     $('.show').on('click',function(){
-        $('.show-out').show()
+        $('.show-out').show(900)
     });
+
     $('#data').on('click',function(){
-        console.log($(this).closest('li'));
-        $(this).each(function () {
-            var Width=$(this).width(),
-                Height=$(this).height(),
-                Pos=$(this).position(),
-                Text=$(this).text(),
-                Att;
+        var Height=$(this).outerHeight(),
+            Width=$(this).outerWidth(),
+            Pos=$(this).offset(),
             Att = $.map(this.attributes, function (attribute) {
                 Att = ' '+attribute.name+' = '+attribute.value;
                 return Att;
-            });
-            console.log(" Width "+Width +"; Height: "+Height+"; Top: "+Pos.top+"; Left: "+Pos.left+" Att:"+Att+" Text: "+Text);
-        });
+            }),
+            Par=$(this).parent().prop('tagName'),
+            Prev=$(this).prev().prop("outerHTML"),
+            Next=$(this).next().prop("outerHTML"),
+            Text=$(this).text();
+        console.log("Height: "+Height+"px"+"; Width "+Width +"px"+"; Top: "+Pos.top+"px"+"; Left: "+Pos.left+"px"+"; Att:"+Att+"; ParTag: <"+Par.toLowerCase()+">; PrevElem: "+Prev+"; NextElem: "+Next+"; Text: "+Text);
     });
+
+});
+
+//form
+$(document).ready(function () {
+
+    $('form :input').change(function () {
+        console.log($(this).val());
+    });
+
+    $('form').submit(function() {
+        console.log($(this).serializeArray());
+        return false;
+    });
+
+    $('#born').keyup(function(){
+        console.log($(this).val());
+    });
+
+    $('#status').keyup(function(){
+        $('#soc').val($(this).val());
+        console.log($(this).val());
+    });
+
+    $('#from').keyup(function(){
+        $(this).val($(this).val().toUpperCase());
+    });
+
 });
