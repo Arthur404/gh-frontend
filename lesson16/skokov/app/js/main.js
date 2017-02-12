3745680236c18de8c69ad4a0e55506d3f9e77d09
Arthur404 <ananarben@gmail.com>
2017-02-12 14:39:47 +0200

    skokov final

--- lesson16/skokov/app/js/main.js
+++ lesson16/skokov/app/js/main.js
@@ -1,10 +1,51 @@
-//slider
+$(document).ready(function(){
 $('.slider').slick({
     dots: true,
     arrows: true,
-    cssEase: 'ease-in'
+    cssEase: 'ease-in',
+    responsive: [
+        {
+            breakpoint: 810,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
 });
 
 $('#open-nav').click(function(){
     $('.navigation').slideToggle(500);
+});
+
+    $('.isotope').isotope({
+        itemSelector: '.masonry',
+        percentPosition: true,
+        isFitWidth: true,
+        masonry: {
+            columnWidth: 1
+        }
+    });
+
+    $('.slider-client').slick({
+        arrows: true,
+        cssEase: 'ease-in',
+        slidesToShow: 8,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
